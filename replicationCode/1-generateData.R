27adcde07b09a1449c5e5ecd2bb88072885ec19e
theo-s <theo_s@sonic.net>
2018-09-24 21:02:57 -0700

    Add modularity for linear model generation

--- replicationCode/1-generateData.R
+++ replicationCode/1-generateData.R
@@ -2,9 +2,11 @@ library(forestry)
 library(MASS)
 
 # Generate several simulated datasets to run estimators on
+# Models 1 and 3 n = 506, p = 13
 set.seed(58)
 x <- Boston[,-14]
 y <- Boston[,14]
+
 
 #   Model 1: LM-RF Hybrid
 ridge <- forestry(x = x, y = y, nodesizeStrictSpl = 25  ,ridgeRF = TRUE, overfitPenalty = 1.5)
@@ -12,13 +14,18 @@ medval <- predict(ridge, x) + rnorm(nrow(x), sd = 2)
 
 lmrf <- cbind(x, medval)
 
-#   Model 2: Purely Linear Model (p = 8, n = 500)
-b <- matrix(runif(8,-1,1), nrow = 8, ncol = 1)
-b[c(2,5),] = 0
-b
-x <- matrix(rnorm(8*500), nrow = 500, ncol = 8)
+#   Model 2: Purely Linear Model (Specify number of features linear on)
 
-y <- x %*% b + rnorm(500)
+n <- 500
+p <- 8
+nonlinear.feats <- 6
+
+b <- matrix(runif(p,-1,1), nrow = p, ncol = 1)
+b[sample(1:p, nonlinear.feats),] = 0
+
+x <- matrix(rnorm(p*n), nrow = n, ncol = 8)
+
+y <- x %*% b + rnorm(n)
 x <- as.data.frame(x)
 lm <- cbind(x, y)
 
