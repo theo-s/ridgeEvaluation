5a8e3d06e1f8c12e05e72a410b678536cc95c578
theo-s <theo_s@sonic.net>
2019-02-02 16:46:58 -0800

    Clean up caret tuning

--- replicationCode/2-generateEstimators.R
+++ replicationCode/2-generateEstimators.R
@@ -9,57 +9,46 @@ library(caret)
 # Define all estimators:
 
 estimator_grid <- list(
-  "tunedRidgeRF" = function(Xobs, Yobs) {
+  "caretRidgeRF" = function(Xobs, Yobs) {
     ridgeRF <- list(type = "Regression",
                     library = "forestry",
-                    loop = NULL) 
-    
-    params <- data.frame(parameter = c("mtry", "nodesizeSpl", "overfitPenalty"),
-                         class = rep("numeric", 3),
-                         label = c("mtry", "nodesizeSpl", "overfitPenalty"))
-    
-    ridgeGrid <- function(x, y, len = 1, search = "random") {
-      ## Define ranges for the parameters and
-      ## generate random values for them
-      
-      grid <- data.frame(mtry = sample(1:ncol(x), 
-                                       len, 
-                                       replace = TRUE),
-                         nodesizeSpl = ceiling(exp(runif(len, 
-                                                         min = log(5),
-                                                         max = log(.25*nrow(x))))),
-                         # Might want to pass specific range/distribution for lambdas ?? 
-                         overfitPenalty = exp(runif(len, 
-                                                    min = log(.05), 
-                                                    max = log(15)))
-      )
-      grid
-    }
-    
-    ridgeFit <- function(x, y, wts, param, lev = NULL, last, weights, classProbs) { 
-      forestry(x = x, 
-               y = y, 
-               ridgeRF = TRUE, 
-               nodesizeSpl = param$nodesizeSpl, 
-               nodesizeStrictSpl = 1,
-               mtry = param$mtry, 
-               overfitPenalty = param$overfitPenalty)
-    }
-    
-    ridgePred <- function(modelFit, newdata, preProc = NULL, submodels = NULL) {
-      predict(modelFit, newdata)
-    }
-    
-    ridgeProb <- function(modelFit, newdata, preProc = NULL, submodels = NULL) {
-      foresty::predict(modelFit, newdata, type = "probabilities")
-    }
-    
-    
-    ridgeRF$parameters <- params
-    ridgeRF$grid <- ridgeGrid
-    ridgeRF$fit <- ridgeFit
-    ridgeRF$predict <- ridgePred
-    ridgeRF$prob <- ridgeProb
+                    loop = NULL,
+                    params = data.frame(parameter = c("mtry", "nodesizeStrictSpl", "overfitPenalty"),
+                                        class = rep("numeric", 3),
+                                        label = c("mtry", "nodesizeStrictSpl", "overfitPenalty")),
+                    grid = function(x, y, len = 1, search = "random") {
+                      ## Define ranges for the parameters and
+                      ## generate random values for them
+                      
+                      paramGrid <- data.frame(mtry = sample(1:ncol(x), 
+                                                            len, 
+                                                            replace = TRUE),
+                                              nodesizeStrictSpl = ceiling(exp(runif(len, 
+                                                                                    min = log(5),
+                                                                                    max = log(.25*nrow(x))))),
+                                              # Might want to pass specific range/distribution for lambdas
+                                              overfitPenalty = exp(runif(len, 
+                                                                         min = log(.05), 
+                                                                         max = log(15)))
+                      )    
+                      return(paramGrid) 
+                    },
+                    fit = function(x, y, wts, param, lev = NULL, last, weights, classProbs) { 
+                      forestry(x = x, 
+                               y = y, 
+                               ridgeRF = TRUE, 
+                               nodesizeSpl = 1, 
+                               nodesizeAvg = 1,
+                               nodesizeStrictAvg = 1,
+                               nodesizeStrictSpl = param$nodesizeStrictSpl,
+                               mtry = param$mtry, 
+                               overfitPenalty = param$overfitPenalty,
+                               saveable = FALSE)
+                    },
+                    predict = function(modelFit, newdata, preProc = NULL, submodels = NULL) {
+                      predict(modelFit, newdata)
+                    },
+                    prob= NULL)
     
     fitControl <- trainControl(method = "repeatedcv",
                                ## 5-fold CV
@@ -67,8 +56,7 @@ estimator_grid <- list(
                                ## repeated 5 times
                                repeats = 10,
                                adaptive = list(min = 5, alpha = 0.05, 
-                                               method = "gls", complete = TRUE)
-    )
+                                               method = "gls", complete = TRUE))
     
     rf <- train(Yobs ~.,
                 data = cbind(Xobs, Yobs), 
@@ -76,24 +64,24 @@ estimator_grid <- list(
                 metric = "RMSE",
                 tuneLength = 10,
                 trControl = fitControl)
-    rf
+    return(rf)
   },
   
-
+  
   "forestry" = function(Xobs, Yobs, lambda)
     forestry(Xobs, Yobs),
   
   "ranger" = function(Xobs, Yobs)
     ranger(Yobs ~., data = cbind(Xobs, Yobs)),
-
+  
   
   "glmnet" = function(Xobs, Yobs)
     glmnet(x = data.matrix(Xobs), y = Yobs),
-
+  
   
   "local_RF" = function(Xobs, Yobs)
     local_linear_forest(X = Xobs, Y = Yobs, num.trees = 500),
-
+  
   
   "cubist" = function(Xobs, Yobs)
     cubist(x = Xobs, y = Yobs)
@@ -101,119 +89,34 @@ estimator_grid <- list(
 
 
 predictor_grid <- list(
-  "ridge_1" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_2" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_3" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_4" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_5" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_6" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_7" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_8" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_9" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "ridge_10" = function(estimator, feat) {
+  "caretRidgeRF" = function(estimator, feat) {
     return(predict(estimator, feat))
   },
   
-  "forestry_1" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "forestry_2" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "forestry_3" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "forestry_4" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "forestry_5" = function(estimator, feat) {
-    return(predict(estimator, feat))
-  },
-  "forestry_6" = function(estimator, feat) {
+  
+  "forestry" = function(estimator, feat) {
     return(predict(estimator, feat))
   },
   
-  "ranger_1" = function(estimator, feat) {
-    return(predict(estimator, feat)$predictions)
-  },
-  "ranger_2" = function(estimator, feat) {
-    return(predict(estimator, feat)$predictions)
-  },
-  "ranger_3" = function(estimator, feat) {
-    return(predict(estimator, feat)$predictions)
-  },
-  "ranger_4" = function(estimator, feat) {
-    return(predict(estimator, feat)$predictions)
-  },
-  "ranger_5" = function(estimator, feat) {
+  
+  "ranger" = function(estimator, feat) {
     return(predict(estimator, feat)$predictions)
   },
   
-  "glmnet_1" = function(estimator, feat) {
+  
+  "glmnet" = function(estimator, feat) {
     feat <- data.matrix(feat)
     l <- estimator$lambda[estimator$lambda == min(estimator$lambda)]
     return(predict(estimator, s = l, newx = feat))
   },
-  "glmnet_2" = function(estimator, feat) {
-    feat <- data.matrix(feat)
-    l <- estimator$lambda[estimator$lambda == min(estimator$lambda)]
-    return(predict(estimator, s = l, newx = feat))
-  },
-  "glmnet_3" = function(estimator, feat) {
-    feat <- data.matrix(feat)
-    l <- estimator$lambda[estimator$lambda == min(estimator$lambda)]
-    return(predict(estimator, s = l, newx = feat))
-  },
-  "local_RF_1" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_2" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_3" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_4" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_5" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_6" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_7" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_8" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_9" = function(estimator, feat) {
-    return(predict(estimator, newdata = feat)$predictions)
-  },
-  "local_RF_10" = function(estimator, feat) {
+  
+  
+  "local_RF" = function(estimator, feat) {
     return(predict(estimator, newdata = feat)$predictions)
   },
   
-  "cubist_1" = function(estimator, feat) {
+  
+  "cubist" = function(estimator, feat) {
     return(predict(estimator, feat))
   }
   
