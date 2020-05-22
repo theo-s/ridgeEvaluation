39dccaf7775a713f6eea1023784346f06ecde7f3
theo-s <theo_s@sonic.net>
2020-05-21 18:17:09 -0700

    Simple V example

--- replicationCode/13-generateComparativeExample.R
+++ replicationCode/13-generateComparativeExample.R
@@ -14,11 +14,11 @@ noise_less_y <- ifelse(x > 1, -2*x+ 4, 2*x )
 y <- noise_less_y + rnorm(n, sd = .3) 
 df <- data.frame(x,y)
 
-# Locally linear data set
+# Locally linear data set ------------------------------------------------------
 #plot <- ggplot(data = df, aes(x = x, y = y)) + geom_point(shape = 1) + theme_classic()
 
 
-# Standard CART tree
+# Standard CART tree -----------------------------------------------------------
 rf <- forestry(x = x, 
                y = y,
                nodesizeStrictSpl = 5,
@@ -29,7 +29,7 @@ rf_df <- data.frame(x, rf_out)
 #ggplot(data = rf_df, aes(x = x, y = rf_out)) + geom_point(shape = 1) + theme_classic()
 
 
-# Linear CART tree
+# Linear CART tree -------------------------------------------------------------
 lrf <- forestry(x = x, 
                 y = y,
                 minSplitGain = .1,
@@ -43,7 +43,7 @@ lrf_df <- data.frame(x, lrf_out$predictions)
 #ggplot(data = lrf_df, aes(x = x, y = lrf_out.predictions)) + geom_point(shape = 1) + theme_classic()
 
 
-# Local Linear Forest tree
+# Local Linear Forest tree -----------------------------------------------------
 llf <- ll_regression_forest(X = as.matrix(x), 
                             Y = y, 
                             num.trees = 1,
@@ -53,7 +53,7 @@ llf_out <- predict(llf, as.data.frame(x))$predictions
 llf_df <- data.frame(x, llf_out)
 #ggplot(data = llf_df, aes(x = x, y = llf_out)) + geom_point(shape = 1, color = "red") + geom_line() + theme_classic()
 
-# Retrieve Split points and coefficients
+# Retrieve Split points and coefficients ---------------------------------------
 plot <- data.frame(
   x = x, 
   Truth = noise_less_y, 
@@ -84,7 +84,7 @@ plot + theme_classic() +
   scale_x_continuous(breaks = seq(0, 2, length.out = 5) + 1) +
   coord_cartesian(ylim = c(-1, 3), xlim = c(0, 2.1)) + ggtitle(label = "CART and Linear Aggregation Comparison")
 
-# We can compare the complexity of the trees 
+# We can compare the complexity of the trees -----------------------------------
 plot(rf, tree.id = 1)
 plot(lrf, tree.id = 1)
 
@@ -102,7 +102,7 @@ y <- noise_less_y + rnorm(n, sd = .15)
 plot(x, y)
 df <- data.frame(x,y)
 
-# Standard CART tree
+# Standard CART tree -----------------------------------------------------------
 rf <- forestry(x = x, 
                y = y,
                nodesizeStrictSpl = 5,
@@ -111,7 +111,7 @@ rf <- forestry(x = x,
 rf_out <- predict(rf, x)
 rf_df <- data.frame(x, rf_out)
 
-# Linear CART tree
+# Linear CART tree -------------------------------------------------------------
 lrf <- forestry(x = x, 
                 y = y,
                 minSplitGain = .1,
@@ -123,7 +123,7 @@ lrf <- forestry(x = x,
 lrf_out <- predict(lrf, x, aggregation = "coefs")
 lrf_df <- data.frame(x, lrf_out$predictions)
 
-# Local Linear Forest tree
+# Local Linear Forest tree -----------------------------------------------------
 llf <- ll_regression_forest(X = as.matrix(x), 
                             Y = y, 
                             num.trees = 1,
@@ -132,7 +132,7 @@ llf <- ll_regression_forest(X = as.matrix(x),
 llf_out <- predict(llf, as.data.frame(x))$predictions
 llf_df <- data.frame(x, llf_out)
 
-# Retrieve Split points and coefficients
+# Retrieve Split points and coefficients ---------------------------------------
 plot <- data.frame(
   x = x, 
   Truth = noise_less_y, 
@@ -163,8 +163,57 @@ plot + theme_classic() +
   scale_x_continuous(breaks = seq(0, 6, length.out = 5) + 1) +
   coord_cartesian(ylim = c(-1, 3), xlim = c(0, 6.1)) + ggtitle(label = "CART and Linear Aggregation Comparison #2")
 
-# We can compare the complexity of the trees and see how simple Ridge tree is
+# We can compare the complexity of the trees -----------------------------------
 plot(rf, tree.id = 1)
 plot(lrf, tree.id = 1)
+
+
+# Simple V example ----------------------------------------------------------------
+set.seed(4335)
+n <- 500
+p <- 10
+X <- matrix(rnorm(n*p), n, p)
+Y <- ifelse(X[,1] > 0, 3 * X[,1],  - 3 * X[,1])
+
+
+# GRF Local Linear Forest ------------------------------------------------------
+grf_linear_forest <- ll_regression_forest(
+  X, 
+  Y,
+  mtry = 10,
+  num.trees = 1,
+  enable.ll.split = TRUE,
+  min.node.size = 100)
+
+pred_grf <- predict(grf_linear_forest, X)$predictions
+
+# Forestry Linear Random Forest ---------------------------------------------------------
+forestry_linear_rf <- forestry(
+  X,
+  Y, 
+  mtry = 10,
+  ntree = 1,
+  nodesizeStrictSpl = 100,
+  linear = TRUE
+)
+
+pred_forestry <- predict(forestry_linear_rf, X)
+
+# Plot data --------------------------------------------------------------------
+data.frame(
+  x = X[,1], 
+  Signal = Y, 
+  GRF = pred_grf,
+  Linear_forestry = pred_forestry
+) %>% melt(id = "x") %>%
+  ggplot(aes(x = x, y = value, color = variable)) + 
+  geom_line() +
+  scale_color_manual(values = c("Signal" = "blue", 
+                                "Linear_forestry" = "green", 
+                                "GRF" = "red")) +
+  theme_bw()
+
+ggsave("~/Downloads/lrf_grf_comparison.pdf", width = 8, height = 8)
+
 
 
