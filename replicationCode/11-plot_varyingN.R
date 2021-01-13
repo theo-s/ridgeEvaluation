3700668566d1efd67efb38d3e3ccca832963dcab
theo-s <theo_s@sonic.net>
2021-01-12 17:21:14 -0800

    Redo figure with GGrepel for labels

--- replicationCode/11-plot_varyingN.R
+++ replicationCode/11-plot_varyingN.R
@@ -25,7 +25,6 @@ library(viridis)
 
 X <- read.csv("replicationCode/9-run_all_cluster_resultsEMSE.csv", stringsAsFactors = FALSE)
 # X <- X[22:36,-1]
-X <- X[,c(-1,-5)]
 X$n <- as.numeric(gsub(pattern = "([^0123456789])", replacement = "", X$Dataset))
 X$Dataset <- gsub(pattern = "([-0123456789])", replacement = "", X$Dataset)
 X <- melt(data = X, id.vars = c("Dataset", "n"))
@@ -54,7 +53,8 @@ X %>% filter(!is.na(value)) %>%
 
 library(ggrepel)
 
-for (expnm in unique(X$dsname)) {
+# Plot experiment 1
+for (expnm in c("Experiment 1")) {
   X %>% 
     filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>% 
     dplyr::select(c(4)) %>% 
@@ -71,12 +71,15 @@ for (expnm in unique(X$dsname)) {
     #geom_text(aes(label = variable)) +
     theme_bw() + 
     xlim(0, 2500) +
-    ylim(0, max + .1)+
+    ylim(-.2, max + .1)+
     theme(legend.position = "none") +
     geom_text_repel(
-      aes(label = variable), data = end_values,
-      color = "black", size = 3, force = 12,
-      direction = "both", nudge_x = 50, nudge_y = .1, point.padding = 1
+      aes(label = variable),
+      data = end_values, color = c("#440154FF", "#443A83FF",
+                                   "#31688EFF", "#21908CFF",
+                                   "#35B779FF", "#8FD744FF", "#FDE725FF"),
+      size = 3, force = 6,arrow = arrow(length = unit(0.01, "npc")),
+      direction = "both", nudge_x = 80, nudge_y = .1, point.padding = .9
     )+
     # ggtitle(label = expnm) +
     ggtitle(label = "") +
@@ -92,3 +95,85 @@ for (expnm in unique(X$dsname)) {
 }
 
 
+# Plot experiment 2
+for (expnm in c("Experiment 2")) {
+  X %>% 
+    filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>% 
+    dplyr::select(c(4)) %>% 
+    max() -> max
+  
+  X %>% 
+    filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>% 
+    dplyr::select(-Dataset, -dsname) %>% 
+    filter(n == 2048) -> end_values
+  
+  X %>% filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>%
+    ggplot(aes(x = n, y = value, color = variable))  +
+    geom_line() +
+    #geom_text(aes(label = variable)) +
+    theme_bw() + 
+    xlim(0, 2500) +
+    ylim(-.1, max + .1)+
+    theme(legend.position = "none") +
+    geom_text_repel(
+      aes(label = variable),
+      data = end_values, color = c("#440154FF", "#443A83FF",
+                                   "#31688EFF", "#21908CFF",
+                                   "#35B779FF", "#8FD744FF", "#FDE725FF"),
+      size = 3, force = 3,arrow = arrow(length = unit(0.01, "npc")),
+      direction = "both", nudge_x = 25, nudge_y = 0, point.padding = .8
+    )+
+    # ggtitle(label = expnm) +
+    ggtitle(label = "") +
+    xlab("Sample Size") +
+    ylab("EMSE") + 
+    geom_point() + 
+    scale_color_viridis_d()
+  
+  ggsave(file = paste0("figures/varyn_", expnm, ".pdf"), height = 3.3, width = 6)
+  #ggsave(file = paste0("~/Dropbox/RidgeForestry_paper/figures/2-VaryNSim/varyn_", 
+  #                     expnm, ".pdf"), height = 3.3, width = 6)
+  
+}
+
+
+# Plot experiment 3
+for (expnm in c("Experiment 3")) {
+  X %>% 
+    filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>% 
+    dplyr::select(c(4)) %>% 
+    max() -> max
+  
+  X %>% 
+    filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>% 
+    dplyr::select(-Dataset, -dsname) %>% 
+    filter(n == 2048) -> end_values
+  
+  X %>% filter(!is.na(value) & dsname == expnm & variable != "cubist" & variable != "caretRidgeTree") %>%
+    ggplot(aes(x = n, y = value, color = variable))  +
+    geom_line() +
+    #geom_text(aes(label = variable)) +
+    theme_bw() + 
+    xlim(0, 2500) +
+    ylim(-.1, max + .1)+
+    theme(legend.position = "none") +
+    geom_text_repel(
+      aes(label = variable),
+      data = end_values, color = c("#440154FF", "#443A83FF",
+                                   "#31688EFF", "#21908CFF",
+                                   "#35B779FF", "#8FD744FF", "#FDE725FF"),
+      size = 3, force = 3,arrow = arrow(length = unit(0.01, "npc")),
+      direction = "both", nudge_x = 25, nudge_y = 0, point.padding = .92
+    )+
+    # ggtitle(label = expnm) +
+    ggtitle(label = "") +
+    xlab("Sample Size") +
+    ylab("EMSE") + 
+    geom_point() + 
+    scale_color_viridis_d()
+  
+  ggsave(file = paste0("figures/varyn_", expnm, ".pdf"), height = 3.3, width = 6)
+  #ggsave(file = paste0("~/Dropbox/RidgeForestry_paper/figures/2-VaryNSim/varyn_", 
+  #                     expnm, ".pdf"), height = 3.3, width = 6)
+  
+}
